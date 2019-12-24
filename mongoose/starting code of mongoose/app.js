1723b97090dadc3c32993b3cb4eec0a7d182ba0f
yasir ahmed <yasira640@gmail.com>
2019-12-25 04:44:14 +0500

    mongoose video 217 done

--- mongoose/starting code of mongoose/app.js
+++ mongoose/starting code of mongoose/app.js
@@ -3,8 +3,11 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const mongoose = require('mongoose');
+
+
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
+// const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
 
 const app = express();
@@ -19,9 +22,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById('5baa2528563f16379fc8a610')
+  User.findById('5e02a25f8cb7252e904b9d2b')
     .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
+      req.user = user;
       next();
     })
     .catch(err => console.log(err));
@@ -32,6 +35,26 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-});
+// mongoConnect(() => {
+//   app.listen(3000);
+// });
+mongoose
+  .connect('mongodb+srv://user101:8wjw1HpFJZvd8kd4@cluster0-56yha.mongodb.net/shop?retryWrites=true&w=majority')
+  .then((res) => {
+    User.findOne().then((user) => {
+
+      if (!user) {
+        const user = new User({
+          name: 'Yasir',
+          email: 'yasira640@gmail.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save()
+      }
+    })
+    app.listen(3000);
+  }).catch((err) => {
+    console.log(err)
+  })
